00fa03baf642108e5125a2e2ff1b42a26c84744f
passy4ucj <ojinnnakapascal@gmail.com>
2021-02-09 14:18:45 +0100

    Create a new Gig record

--- routes/gigsRoutes.js
+++ routes/gigsRoutes.js
@@ -4,6 +4,8 @@ const Gig = require('../models/Gig')
 
 const router = express.Router()
 
+
+// Get gigs
 router.get('/', async(req, res) => {
     try {
         const gigs = await Gig.findAll()
@@ -15,6 +17,38 @@ router.get('/', async(req, res) => {
     } catch (error) {
         console.log('Error :', error)
     }
+   
+})
+
+
+// Add a Gig
+router.get('/add', async(req, res) => {
+    const data = {
+        title: 'SImple Wordpress developer',
+        technologies: 'wordpress',
+        budget: '$300',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+        contact_email: 'passy@example.com'
+    }
+
+    let { title, technologies, budget, description, contact_email } = data
+    try {
+        // Insert into table
+        await Gig.create({
+            title,
+            technologies,
+            description,
+            budget,
+            contact_email
+        })
+    
+        res.redirect('/gigs')
+
+    } catch (error) {
+        console.log('Error: ', error)
+    }
+
+    
    
 })
 
